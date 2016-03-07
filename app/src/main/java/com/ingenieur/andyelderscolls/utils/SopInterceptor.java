cc42503ecb290d92043eaa2a97270dd3ea2d52ee
phil <philjord@ihug.co.nz>
2016-03-07 23:16:22 +0100

    Many change all is in flux

---
+++ app/src/main/java/com/ingenieur/andyelderscolls/utils/SopInterceptor.java
@@ -0,0 +1,27 @@
+package com.ingenieur.andyelderscolls.utils;
+
+import android.util.Log;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+/**
+ * Created by phil on 3/7/2016.
+ */
+public class SopInterceptor extends PrintStream
+{
+	String tag;
+
+	public SopInterceptor(OutputStream out, String tag)
+	{
+		super(out, true);
+		this.tag = tag;
+	}
+
+	@Override
+	public void print(String s)
+	{//do what ever you like
+		//super.print(s);
+		Log.w(tag, s);
+	}
+}
