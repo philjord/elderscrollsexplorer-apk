0add838e265e64222eee68adfdd2ede0a4b99fcd
Philip Jordan <p.j.nz@outlook.com>
2019-10-20 09:52:44 +1300

    crashlytics awas not configured correctly

--- app/build/generated/source/buildConfig/debug/com/ingenieur/andyelderscrolls/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/ingenieur/andyelderscrolls/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ingenieur.ese.eseandroid";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 137;
-  public static final String VERSION_NAME = "1.37";
+  public static final int VERSION_CODE = 138;
+  public static final String VERSION_NAME = "1.38";
 }
